1165cd9866e64f8016fddbd12f62139586cc4fa1
Johannes Heck <yoyostile@gmail.com>
2015-10-21 16:40:54 +0200

    add sample yml

--- lib/generators/fluent_helpers_generator.rb
+++ lib/generators/fluent_helpers_generator.rb
@@ -2,6 +2,7 @@ class FluentHelpersGenerator < Rails::Generators::Base
   source_root File.expand_path(File.dirname(__FILE__))
 
   def copy_initializer
+    copy_file "fluent_helpers.yml", "config/fluent_helpers.yml"
     copy_file "fluent_helpers.rb", "config/initializers/fluent_helpers.rb"
   end
 end
